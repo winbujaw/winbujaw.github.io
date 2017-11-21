52dbfda96fd19957aa316c273613c2c06847cfe4
winbujaw <30627869+winbujaw@users.noreply.github.com>
2017-11-21 09:56:33 -0500

    Add files via upload

---
+++ tribute/tribute.js
@@ -0,0 +1,22 @@
+        
+                $("#Achievements").click(function(){
+                    $(".ul").toggle(1000);
+                        });
+                           
+         
+          
+                $("#Life").click(function(){
+                    $(".ol").toggle(1000);
+                        });
+                            
+         
+            
+                    $("#Childhood").click(function(){
+                        $(".Childhood").toggle(1000);
+                            });
+                               
+         
+             
+               
+         
+    
