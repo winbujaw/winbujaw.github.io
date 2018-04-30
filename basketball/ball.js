e864dd245d08b7631fa8618adf72d1bab15e5aec
winbujaw <30627869+winbujaw@users.noreply.github.com>
2018-04-30 09:11:29 -0400

    Update ball.js

--- basketball/ball.js
+++ basketball/ball.js
@@ -1,9 +1,6 @@
 
 var paddle;
 var paddle2;
-
-
-
 var ball;
 var game;
 var box;
@@ -23,9 +20,9 @@ function checkCollisions() {
         }
         
         //Distance Collision
-        dist = box.distanceTo(character);
+        dist = paddle.distanceTo(character);
         output.innerHTML = dist;
-        if(dist - character.width/2 - box.width/2 < 0){
+        if(dist - character.width/2 - paddle.width/2 < 0){
             output.innerHTML = dist;
         character.dx *= -1;
         character.dy *= -1;
@@ -65,6 +62,7 @@ function init() {
     
     
     scene = new Scene();
+     game.hideCursor();
     scene.setSize(600,300);
     paddle = new Paddle();
     paddle2 = new Paddle();
@@ -75,6 +73,9 @@ function init() {
         this.setX(document.mouseX);
         this.setY(document.mouseY);
     }*/
+    
+    output = document.getElementById("output");
+    
     paddle.setPosition(15, scene.height / 2);
     paddle2.setPosition(scene.width - 20, scene.height / 2);
     ball = new Sprite(scene, "ball.png" ,100, 100);
@@ -88,9 +89,10 @@ function init() {
     
     
     scene.start();
-    box = new Sprite(game, "court.png", 100, 100);
-    box.setSpeed(0);
-    box.setPosition(game.width/2, game.height/2);
+    EPICMUSIC.play();
+    paddle = new Sprite(game, "court.png", 100, 100);
+    paddle.setSpeed(0);
+    paddle.setPosition(game.width/2, game.height/2);
     
     
     character.setSpeed(5);
@@ -107,12 +109,14 @@ function init() {
 
 
 function update() {
-    
-    scene.clear();
     paddle.checkKeys();
-    
-    paddle2.update();
-    paddle.update();
     ball.update();
+    scene.clear();
+    paddle.update();
+    paddle2.update();
+   
+    
+    character.update();
     checkCollisions();
+    //character.followMouse();
 }
